5344cd072fa8a6d86622b50ad378bf355b552989
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-25 21:33:26 -0500

    made delete button functional

--- main.js
+++ main.js
@@ -9,6 +9,24 @@ const printToDom = (divId, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 };
 
+const deleteFunction = (e) => {
+    const buttonId = e.target.id;
+    ingredients.forEach((ingredient, index) => {
+        if(ingredient.id === buttonId){
+            ingredients.splice(index, 1);
+        }
+    })
+    domStringBuilder(ingredients);
+    addDeleteEvents();
+};
+
+const addDeleteEvents = () => {
+    const deleteButtons = document.getElementsByClassName('deleteButton');
+    for (let i = 0; i < deleteButtons.length; i++){
+        deleteButtons[i].addEventListener('click', deleteFunction);
+    }
+};
+
 const domStringBuilder = (arrayToPrint) => {
     let domString = '';
     arrayToPrint.forEach((ingredient) => {
@@ -35,6 +53,7 @@ const addIngredient = (e) => {
     ingredients.push(newIngredient);
     ingredientCounter++;
     domStringBuilder(ingredients);
+    addDeleteEvents();
     inputIngredient.value = '';
 
 };
